27c3b531025982fd5df2df68cf11f6dad7443613
fengqi <feng.qi@msn.com>
2017-10-10 22:14:48 +0800

    update

--- install.sh
+++ install.sh
@@ -7,16 +7,31 @@ COLR_RED='\033[0;31m'
 COLR_GREEN='\033[0;32m'
 COLR_NC='\033[0m' # No Color
 
+WARN='\033[0;31m[WARN]\033[0m'
+INFO='\033[0;32m[WARN]\033[0m'
+
 command_exists () {
     type "$1" &> /dev/null ;
 }
 
+package_installed () {
+    dpkg -s "$1" &> /dev/null ;
+    # dpkg-query -l "$1" &>/dev/null ;
+}
+
 install_utils() {
-    if ! command_exists ag ; then
-        sudo apt-get install -y silversearcher-ag
-    else
-        echo "[WARN] ag already exists! Skipped!"
-    fi
+    pkgs_str="git clang clang-format golang-any emacs25 vim-gui-common i3-wm ibus-table-wubi ibus-pinyin chromium-browser zsh yakuake command-not-found"
+    pkgs=(${pkgs_str})
+
+    for pkg in ${pkgs[@]}; do
+        # if ! command_exists ${pkg} ; then
+        if ! package_installed ${pkg} ; then
+            # sudo apt-get install -y silversearcher-ag
+            echo -e "${INFO} ${COLR_GREEN}${pkg}${COLR_NC} will be installed."
+        else
+            echo -e "${WARN} ${COLR_RED}${pkg}${COLR_NC} already exists! Skipped!"
+        fi
+    done
 }
 
 clone_repo() {
@@ -42,8 +57,29 @@ create_soft_link() {
     done
 }
 
+install_fonts() {
+    if ! [ -e "${HOME}/github/fonts" ]; then
+        git clone feng-qi@bitbucket.org:feng-qi/fonts.git ${HOME}/github/fonts
+    fi
+
+    if [ -e "/usr/local/share/fonts/source_code_pro" ]; then
+        echo -e "${WARN} fonts already installed!"
+    else
+        sudo mkdir /usr/local/share/fonts/source_code_pro
+        sudo cp ${HOME}/github/fonts/source-code-pro/* /usr/local/share/fonts/source_code_pro
+        sudo mkdir /usr/local/share/fonts/adobe
+        sudo cp ${HOME}/github/fonts/adobe/* /usr/local/share/fonts/adobe
+        fc-cache -fv
+        echo -e "${INFO} fonts installed."
+    fi
+}
+
 #----- main ----------
 
+install_fonts
 install_utils
 clone_repo
 create_soft_link
+
+# echo "source /etc/zsh_command_not_found" >> $HOME/.zshrc
+# sudo update-command-not-found
