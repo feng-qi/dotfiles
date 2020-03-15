a3cba4ecc8455671cf56b63daf23adc0e186109d
fengqi <feng.qi@msn.com>
2020-03-15 13:45:18 +0800

    prepare for dotfile switch: reorganize files

---
+++ .script/install.sh
@@ -0,0 +1,16 @@
+git clone --bare https://feng-qi@bitbucket.org/feng-qi/dotfiles.git $HOME/.dotfiles
+function config {
+    git --git-dir=$HOME/.dotfiles --work-tree=$HOME $@
+}
+mkdir -p .dotfile-backup
+echo "Checking out dotfiles ..."
+if config checkout; then
+    echo "Done checking out dotfiles"
+else
+    echo "Backing up existing dot files ..."
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfile-backup/{}
+    echo "Done backing up existing dot files"
+    config checkout
+    echo "Done checking out dotfiles"
+fi;
+config config status.showUntrackedFiles no
