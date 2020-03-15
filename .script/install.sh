b1f32d522d81b97427c70e1901fce23d85b9a5f0
fengqi <feng.qi@msn.com>
2020-03-15 17:26:50 +0800

    exit if dotfiles or backup directory already exists in $HOME

--- .script/install.sh
+++ .script/install.sh
@@ -1,12 +1,18 @@
-git clone --bare https://gitee.com/feng-qi/dotfiles.git $HOME/dotfiles
+git clone --bare https://gitee.com/feng-qi/dotfiles.git $HOME/dotfiles || exit 1
 function config { git --git-dir=$HOME/dotfiles --work-tree=$HOME $@; }
-mkdir -p .dotfile-backup/{,.config}
 echo "Checking out dotfiles ..."
 if config checkout; then
     echo "Done checking out dotfiles"
 else
     echo "Backing up existing dot files ..."
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfile-backup/{}
+    if [ -e $HOME/.dotfile-backup ]; then
+        echo "$HOME/.dotfile-backup already exists, can't backup files"
+        exit 2
+    fi
+    mkdir -p $HOME/.dotfile-backup/{,.config,.script,.xmonad}
+    mkdir -p $HOME/.dotfile-backup/.config/{awesome,i3,nvim}
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv $HOME/{} $HOME/.dotfile-backup/{}
+    if [ -e $HOME/README.md ]; then mv $HOME/README.md $HOME/.dotfile-backup; fi
     echo "Done backing up existing dot files"
     config checkout
     echo "Done checking out dotfiles"
